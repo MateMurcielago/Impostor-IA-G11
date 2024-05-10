67019dbfd1d58f2f22f222ad8cffb586636597dd
MateMurcielago <galaxyuslol@gmail.com>
2024-05-09 22:33:31 -0300

    Actualización 9/5 por Vianco

--- src/Impostor/ImpostorAgent.java
+++ src/Impostor/ImpostorAgent.java
@@ -1,12 +1,31 @@
 package Impostor;
 
-public class ImpostorAgent {
+import frsf.cidisi.faia.agent.search.SearchBasedAgent;
+import frsf.cidisi.faia.solver.search.UniformCostSearch;
+import frsf.cidisi.faia.agent.Action;
+import frsf.cidisi.faia.agent.search.Problem;
+import frsf.cidisi.faia.agent.search.SearchAction;
+import java.util.Vector;
+import Impostor.Actions.*;
+
+public class ImpostorAgent extends SearchBasedAgent {
 	public ImpostorAgent() {
 		//El gol del impostor
 		ImpostorGoal goal = new ImpostorGoal();
 		
 		ImpostorAgentState estado = new ImpostorAgentState();
-		//this.setAgentState(estado);
+		this.setAgentState(estado);
 		
+		Vector<SearchAction> operadores = new Vector<SearchAction>();
+		operadores.add(new Matar());
+		//operadores.add(new Moverse()); etc
+		
+		Problem problema = new Problem(goal, estado, operadores);
+		this.setProblem(problema);
+	}
+	
+	@Override
+    public Action selectAction() {
+		UniformCostSearch estrategia = new UniformCostSearch();
 	}
 }
